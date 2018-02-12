5f7f6774603ac77ac82ff236354710122c430888
Patrick Huffer <Patrick.Huffer@amtrustfinancial.com>
2018-02-12 16:53:28 -0500

    Clarification on backwards pipelining

--- Chapter5/Pipelining_Backward.fsx
+++ Chapter5/Pipelining_Backward.fsx
@@ -1,5 +1,4 @@
-﻿let fahrenheitToCelsius degreesF = (degreesF - 32.0) * (5.0 / 9.0)
-let marchHighTemps = [ 33.0; 30.0; 33.0; 38.0; 36.0; 31.0; 35.0;
+﻿let marchHighTemps = [ 33.0; 30.0; 33.0; 38.0; 36.0; 31.0; 35.0;
                        42.0; 53.0; 65.0; 59.0; 42.0; 31.0; 41.0;
                        49.0; 45.0; 37.0; 42.0; 40.0; 32.0; 33.0;
                        42.0; 48.0; 36.0; 34.0; 38.0; 41.0; 46.0;
@@ -7,4 +6,5 @@ let marchHighTemps = [ 33.0; 30.0; 33.0; 38.0; 36.0; 31.0; 35.0;
 
 printfn "March Average (F): %f" <| List.average marchHighTemps
 
-// It's just like $ in Haskell
+// It's just like $ in Haskell:
+printfn "March Average (F): %f" (List.average marchHighTemps)
