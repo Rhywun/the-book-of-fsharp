a382ce2cdfc13949b983294cc8e298f2f85f1856
Patrick Huffer <rhywun@rhywun.com>
2018-02-13 07:49:02 -0500

    Middle of ch06

--- Chapter6/__shared.fsx
+++ Chapter6/__shared.fsx
@@ -2,13 +2,14 @@
 
 open System.IO
 
-let private moviesFileName = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "ArnoldMovies.txt")
+let private moviesFileName =
+  System.IO.Path.Combine(__SOURCE_DIRECTORY__, "ArnoldMovies.txt")
 let getMovieStream() = new StreamReader(moviesFileName);
 let movies =
   seq { use r = getMovieStream()
         while not r.EndOfStream do
           let l = r.ReadLine().Split(',')
           yield l.[0], int l.[1] };;
-let linesArray = 
+let linesArray =
   [| use r = getMovieStream()
      while not r.EndOfStream do yield r.ReadLine() |];;
