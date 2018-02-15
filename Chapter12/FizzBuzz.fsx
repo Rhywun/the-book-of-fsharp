44b75429c7df50e6a3e826c01bdec514856028fa
Patrick Huffer <rhywun@rhywun.com>
2018-02-15 07:39:52 -0500

    Middle of ch12

--- Chapter12/FizzBuzz.fsx
+++ Chapter12/FizzBuzz.fsx
@@ -1,19 +1,19 @@
 ﻿type FizzBuzzSequenceBuilder() =
-  member x.Yield(v) =
+  member __.Yield(v) =
     match (v % 3, v % 5) with
     | 0, 0 -> "FizzBuzz"
     | 0, _ -> "Fizz"
     | _, 0 -> "Buzz"
-    | _ -> v.ToString()
-  member x.Delay(f) = f() |> Seq.singleton
-  member x.Delay(f : unit -> string seq) = f()
-  member x.Combine(l, r) =
+    | _    -> v.ToString()
+  member __.Delay(f) = f() |> Seq.singleton
+  member __.Delay(f : unit -> string seq) = f()
+  member __.Combine(l, r) =
     Seq.append (Seq.singleton l) (Seq.singleton r)
-  member x.Combine(l, r) =
+  member __.Combine(l, r) =
     Seq.append (Seq.singleton l) r
 //  member x.Combine(l : string seq, r : string seq) =
 //    Seq.append l r
-  member x.For(g, f) = Seq.map f g
+  member __.For(g, f) = Seq.map f g
 
 let fizzbuzz = FizzBuzzSequenceBuilder();;
 
@@ -35,7 +35,7 @@ fizzbuzz.Delay (fun () ->
 *)
 
 fizzbuzz { for x = 1 to 99 do yield x };;
-fizzbuzz { for x in 1..99 do yield x };;
+fizzbuzz { for x in 1..99 do yield x } |> Seq.take 10 |> Seq.toList;;
 fizzbuzz { yield 1
            yield 2
            for x = 3 to 50 do yield x };;
